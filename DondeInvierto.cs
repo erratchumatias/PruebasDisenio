ab69da59205cfe0bf7def6b8d64ab1cb9935d8b8
erratchumatias <erratchumatias@gmail.com>
2017-12-04 23:25:13 -0300

    Agrego

--- DondeInvierto.cs
+++ DondeInvierto.cs
@@ -59,6 +59,64 @@ namespace PruebasDondeInvierto
             driver.Quit();
         }
 
+        [TestMethod]
+        public void PruebaValorEnCuenta()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "http://dondeinviertodds.azurewebsites.net/";
+            verificationErrors = new StringBuilder();
+
+            driver.Navigate().GoToUrl(baseURL + "/Home");
+            driver.FindElement(By.LinkText("Cuentas")).Click();
+            new SelectElement(driver.FindElement(By.Id("EmpresaSeleccionada"))).SelectByText("Erratchu");
+            driver.FindElement(By.XPath("//input[@value='Seleccionar']")).Click();
+            driver.FindElement(By.XPath("(//a[contains(text(),'Detalles')])[10]")).Click();
+            Assert.AreEqual("400000.00", driver.FindElement(By.XPath("//div[8]")).Text);
+            WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 10));
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void PorracinValuadoEnRoi()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "http://dondeinviertodds.azurewebsites.net/";
+            verificationErrors = new StringBuilder();
+
+            driver.Navigate().GoToUrl(baseURL + "/Home");
+            driver.FindElement(By.LinkText("Indicadores")).Click();
+            new SelectElement(driver.FindElement(By.Id("EmpresaSeleccionada"))).SelectByText("Porracin");
+            new SelectElement(driver.FindElement(By.Id("IndicadorSeleccionado"))).SelectByText("ROI");
+            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
+            Assert.AreEqual("0.30", driver.FindElement(By.XPath("//td[6]")).Text);
+            Assert.AreEqual("0.36", driver.FindElement(By.XPath("//tr[3]/td[6]")).Text);
+            Assert.AreEqual("0.33", driver.FindElement(By.XPath("//tr[4]/td[6]")).Text);
+            Assert.AreEqual("0.04", driver.FindElement(By.XPath("//tr[5]/td[6]")).Text);
+            Assert.AreEqual("0.39", driver.FindElement(By.XPath("//tr[6]/td[6]")).Text);
+            WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 10));
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void FeiguinValuadoEnRoe()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "http://dondeinviertodds.azurewebsites.net/";
+            verificationErrors = new StringBuilder();
+
+            driver.Navigate().GoToUrl(baseURL + "/Home");
+            driver.FindElement(By.LinkText("Indicadores")).Click();
+            new SelectElement(driver.FindElement(By.Id("EmpresaSeleccionada"))).SelectByText("Feiguin");
+            new SelectElement(driver.FindElement(By.Id("IndicadorSeleccionado"))).SelectByText("ROE");
+            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
+            Assert.AreEqual("0.00", driver.FindElement(By.XPath("//td[6]")).Text);
+            Assert.AreEqual("0.01", driver.FindElement(By.XPath("//tr[3]/td[6]")).Text);
+            Assert.AreEqual("0.19", driver.FindElement(By.XPath("//tr[4]/td[6]")).Text);
+            Assert.AreEqual("0.17", driver.FindElement(By.XPath("//tr[5]/td[6]")).Text);
+            WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 10));
+            driver.Quit();
+        }
+
         private string CloseAlertAndGetItsText()
         {
             try
